c3c761692a7e3722cd65a8cb9fd206f835fec0b9
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-06-14 19:22:25 -0300

    concluded

---
+++ DS_06_predecessor_and_successor.py
@@ -0,0 +1,3 @@
+print ('\033[4;33m Enter an integer and find its predecessor and successor')
+numero = int (input('\033[1;31m Enter a number\n'))
+print (f'\033[1;35m88 the predecessor of the number {numero} is {numero -1} and its successor is {numero + 1}')
