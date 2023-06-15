cc5c1df14cd9948beedd5f0a861349d1f2797e19
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-06-14 21:03:58 -0300

    concluded

---
+++ DS_08_arithmetic_average.py
@@ -0,0 +1,5 @@
+print('Find the student`s grade point average')
+n1 = float (input('\033[1;31m enter first note: '))
+n2 = float (input('\033[1;33m Enter second note: '))
+n3 = float (input('\033[4;32m Enter third number: '))
+print(f'\033[04;35m the student`s average is: {(n1 + n2) / 3 :.2f}')
