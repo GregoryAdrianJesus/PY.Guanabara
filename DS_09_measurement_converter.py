8a70f380ab30facc67c32fb1aa69d14f31bde956
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-07-05 22:48:25 -0300

    concluded

---
+++ DS_09_measurement_converter.py
@@ -0,0 +1,19 @@
+print('\033[1;31m Enter a distance in meters and find your conversion')
+m = int(input('Distance in meters:'))
+km = m / 1000
+print (f'\033[4;34m Kilometers: {km}km')
+dam = m / 10 
+print(f'\033[4;33m Decameters: {dam}dam')
+hm = m / 100
+print(f'\033[4;35m hectometers: {hm}hm')
+dm = m * 10
+print(f'\033[4;33m Decimeters: {dm}dm')
+cm = m * 100
+print(f'\033[4;35m Centimeters: {cm}cm')
+mm = m * 1000
+print(f'\033[4;34m Milimeters: {mm}mm')
+
+      
+      
+
+
