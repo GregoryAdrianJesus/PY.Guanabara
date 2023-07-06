1d24f2563e3b4f90c100f647a97a173ac4d20e82
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-07-06 00:10:50 -0300

    concluded

--- DS_10_multiplication_table.py
+++ DS_10_multiplication_table.py
@@ -1,7 +1,9 @@
 print('\033[4;34m <<<Enter a number and find your multiplication table>>>')
 tb = float(input('\033[1;31m enter a number:'))
+print('\033[32m ~' * 15)
 for i in range(1,11):
     r = i * tb
     print(f'{tb} X {i} = {r}')
+print('\033[32m ~' * 15)
 
 
