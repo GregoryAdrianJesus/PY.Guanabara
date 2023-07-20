1c8143deceaea87b3aa32cc0da6f8e2ae8174d34
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-07-20 14:41:03 -0300

    concluded

--- DS_13_product_discount.py
+++ DS_13_product_discount.py
@@ -1,3 +1,4 @@
-print('find out the value of a product with 5% the discount')
-valor = float (input('enter the value of product'))
-produto = 
+print('\033[4;35m find out the value of a product with 5% the discount')
+valor = float (input('\033[4;32m enter the value of product: '))
+produto = 5 * valor / 100
+print(f'You have \033[4;33m R${produto} real of discount')
