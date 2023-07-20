8b5978c73f3cf1be731deb194029d6c2bfb0f403
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-07-20 02:03:47 -0300

    concluded

--- DS_011_exchange.py
+++ DS_011_exchange.py
@@ -1,4 +1,5 @@
 print('\033[4;31m Convert real in dollar')
 real = float(input('\033[1;32m Enter value in real: '))
-cv = real / 4.79
-print(f'\033[1;36m The value intered is equivalent to \033[4;33m ${cv:.2f} dollars')
+d = real / 4.79
+e = real / 5.38
+print(f'\033[1;36m The value intered is equivalent to \033[4;33m ${d:.2f} dollars and \033[4;31m {e:.2f} euros')
--- DS_12_paint_your_house.py
+++ DS_12_paint_your_house.py
@@ -1,3 +1,7 @@
-print('know how many liters of paint it will take to paint your wall')
-altura = float(input('enter the height'))
-largura = float (input('enter the width'))
+print('\033[4;35m know how many liters of paint it will take to paint your wall')
+altura = float(input('\033[1;34m enter the height: '))
+largura = float (input('\033[3;33m enter the width: '))
+area = largura * altura
+tinta = area / 2
+print(f'\033[04;34m Your wall has \033[33m {altura} X\033[35m {largura} with \033[36m {area:.2f}m² area')
+print(f'You need \033[4;33m{tinta:.2f} liters of paint to paint your wall')
