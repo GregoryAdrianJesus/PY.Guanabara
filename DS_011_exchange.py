aea896bca41b68aa2834dd9ac9163cb5d0c87a80
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-07-20 00:50:19 -0300

    conluded

---
+++ DS_011_exchange.py
@@ -0,0 +1,4 @@
+print('\033[4;31m Convert real in dollar')
+real = float(input('\033[1;32m Enter value in real: '))
+cv = real / 4.79
+print(f'\033[1;36m The value intered is equivalent to \033[4;33m ${cv:.2f} dollars')
