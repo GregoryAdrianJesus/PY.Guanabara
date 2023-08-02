e69b75f99cc35142881a5099a0792f1fed246068
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-08-02 19:12:22 -0300

    concluded

---
+++ DS_15_tempeture_converter.py
@@ -0,0 +1,4 @@
+print('\033[4;33m Converter degrees Celsius in Fahrenheit')
+graus = float(input('\033[4;32m Enter the degrees Celsius: C°'))
+fahrenheit = graus * 1.8 + 32
+print(f'\033[4;31m {graus}C° equals the {fahrenheit}°F')
