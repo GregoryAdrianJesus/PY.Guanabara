2bc568600024be702d759e18be924447e3ef89c6
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-08-08 16:31:12 -0300

    concluded

--- DS_16_Car_Rental.py
+++ DS_16_Car_Rental.py
@@ -1,7 +1,7 @@
-print('Find out how much you will pay for a renting a car')
-dia = float(input('How many days using the car?'))
-km = float(input('How many Km you will use?'))
+print('\033[4;32m Find out how much you will pay for a renting a car')
+dia = float(input('How many days using the car? '))
+km = float(input('How many Km you will use? '))
 pd = dia * 60
 pk = km * 0.15
 total = pd + pk
-print(f'you will pay for days used ${pd} and for km used ${pk} totalizing:${total}')
+print(f'\033[4;34m You will pay for the days ${pd} and for km used ${pk} totalizing:${total}')
