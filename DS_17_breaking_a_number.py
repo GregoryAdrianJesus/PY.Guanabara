7c929ce8f063319413dacedbb532d1e13759e1e5
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-08-09 02:26:21 -0300

    Incomplete

---
+++ DS_17_breaking_a_number.py
@@ -0,0 +1,5 @@
+from math import trunc
+print('Turning a broken number into a whole number')
+qb = float(input('enter a number with decimal places: '))
+inteiro = trunc (qb)
+print(f'The number {qb} has its interger part: {inteiro} ')
