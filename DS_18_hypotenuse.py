d50fdc7ff8df98c28c7839eb95db5dc1aacee7b6
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-08-09 19:22:49 -0300

    incomplete

--- DS_18_hypotenuse.py
+++ DS_18_hypotenuse.py
@@ -1,7 +1,7 @@
 from math import pow,sqrt
-print('Find the hypotenuse of a right triangle')
+print('\033[4;34m Find the hypotenuse of a right triangle')
 cateto1 = float(input('Enter length of leg 1°: '))
 cateto2 = float(input('Enter length of leg 2°: '))
 elevado = pow(cateto1,2) + pow(cateto2,2)
 raiz = sqrt(elevado)
-print(f'The right trangle with legs of length {cateto1}cm and {cateto2}cm has a hypotenuse of {raiz}cm')
+print(f'\033[4;35m The right trangle with legs of length {cateto1}cm and {cateto2}cm has a hypotenuse of {raiz}cm')
