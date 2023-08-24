3b768536241c7c5f2cfb2ba57a872fb2d3ee68a0
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-08-24 14:48:28 -0300

    concluded

--- DS_19_Sine_cosine_and_tangent.py
+++ DS_19_Sine_cosine_and_tangent.py
@@ -1,2 +1,10 @@
-import math
-print('Find the sine, cosine and tangent of any angle')
+from math import sin,cos,tan
+print('\033[4;33m Find the sine, cosine and tangent of any angle')
+angulo = float(input('\033[1;32m enter an angle: '))
+seno = sin(angulo)
+print(f'\033[1;32m The sine of the angle is: {seno}')
+cosseno = cos(angulo)
+print(f'the cosine of the angle is: {cosseno}')
+tangente = tan(angulo)
+print(f'The tangent of the angle is: {tangente}')
+
