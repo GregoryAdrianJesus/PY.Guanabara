9623afe6eca3af41a254729fe872f965eddde53d
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-09-04 00:31:54 -0300

    concluded

---
+++ DS_21_Selecting_the_order.py
@@ -0,0 +1,8 @@
+from random import shuffle
+print('\033[2;35m select order of items')
+item1 = input('\033[1;34m enter the item:')
+item2 = input('\033[1;31m enter the second item:')
+item3 = input('\033[1;33m enter the third item:')
+lista = [item1,item2,item3]
+shuffle(lista)
+print(f'The ordem of the changed items is:{lista}')
