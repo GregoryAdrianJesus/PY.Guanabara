411f97f5dc27d0ccba5e2bbaf1fb6299b5208bc9
Gregory_Adrian <adrian0808.jesus@gmail.com>
2023-09-06 19:42:46 -0300

    concluded

--- DS_18_hypotenuse.py
+++ DS_18_hypotenuse.py
@@ -1,7 +1,7 @@
 from math import pow,sqrt
 print('\033[4;34m Find the hypotenuse of a right triangle')
-cateto1 = float(input('Enter length of leg 1°: '))
-cateto2 = float(input('Enter length of leg 2°: '))
+cateto1 = float(input('Enter length of first side: '))
+cateto2 = float(input('Enter length of second side: '))
 elevado = pow(cateto1,2) + pow(cateto2,2)
 raiz = sqrt(elevado)
-print(f'\033[4;35m The right trangle with legs of length {cateto1}cm and {cateto2}cm has a hypotenuse of {raiz}cm')
+print(f'\033[4;35m The right trangle with of length of {cateto1}cm and {cateto2}cm has a hypotenuse of {raiz}cm')
--- DS_21_Selecting_the_order.py
+++ DS_21_Selecting_the_order.py
@@ -5,4 +5,4 @@ item2 = input('\033[1;31m enter the second item:')
 item3 = input('\033[1;33m enter the third item:')
 lista = [item1,item2,item3]
 shuffle(lista)
-print(f'The ordem of the changed items is:{lista}')
+print(f'The order of the changed items is:{lista}')
